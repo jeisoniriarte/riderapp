2d9d25faddbb6c9833b1899fec77fcecde280f89
Jeison Iriarte <jeison.18.efra@gmail.com>
2021-02-10 23:14:51 -0500

    Primer Commit RiderApp

---
+++ android/app/src/main/kotlin/com/impulsetech/riderapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.impulsetech.riderapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:riderapp/AllScreens/loginscreen.dart';
+// import 'package:riderapp/AllScreens/mainscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Taxi Rider App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Brand Regular",
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
